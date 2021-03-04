2efd254dec62fb27c01c2259ba336518cda0038c
sharonhelmut <80032652+sharonhelmut@users.noreply.github.com>
2021-03-04 16:06:09 +0700

    Kode yang sesuai diinginkan

--- tugaspi.js
+++ tugaspi.js
@@ -6,3 +6,15 @@ if (angka >= 0) {
 } else {
   alert('angka ini negatif');
 }
+alert('program memasukan angka positif dan negatif')
+  let angka = parseInt(prompt('masukan angka'))
+
+  if (angka >= 0) {
+    alert('angka ini positif');
+  } else if( angka == 0){
+    alert('angka ini adalah 0');
+  } else if (angka < 0) {
+    alert('angka ini adalah negatif');
+  } else {
+    alert('ini bukan angka');
+  }
